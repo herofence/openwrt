a98ff9c0603ebeb639f5093b5b6f87781cc54070
herofence <113574064+herofence@users.noreply.github.com>
2024-03-19 07:07:09 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.2.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -43,6 +43,9 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone --depth=1 https://github.com/linkease/istore
+git clone --depth=1 https://github.com/linkease/nas-packages
+git clone --depth=1 https://github.com/linkease/nas-packages-luci
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus

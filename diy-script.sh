84ca9dde386a61aa542fe4bbde3028950d0873bf
herofence <113574064+herofence@users.noreply.github.com>
2024-03-19 18:45:26 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -45,7 +45,7 @@ git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone --depth=1 https://github.com/linkease/nas-packages
 git clone --depth=1 https://github.com/linkease/nas-packages-luci
-git clone --depth=1 https://github.com/linkease/istore package/istore
+src-git istore https://github.com/linkease/istore;main
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 

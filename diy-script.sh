4313609a1052e231be4872a718ba9e21e767f5c1
herofence <113574064+herofence@users.noreply.github.com>
2024-10-26 13:11:21 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -118,12 +118,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwal
 # 更改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" ./feeds/luci/collections/luci/Makefile
 
-# 在线用户
-git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-
 # x86 型号只显示 CPU 型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 

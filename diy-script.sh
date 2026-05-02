db8cc0345e2cc33f8cae022e3e2411a2394e25b4
herofence <113574064+herofence@users.noreply.github.com>
2026-05-02 09:20:25 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -81,6 +81,7 @@ git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 # git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #VNT
 git clone https://github.com/whzhni1/luci-app-vnt2.git package/vnt2
+git clone https://github.com/cg8-5712/vnt.git package/vnt
 #EasyTier
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 #tailscale
